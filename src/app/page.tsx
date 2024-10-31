80764c81c322b92fc6b4397e08eec403b9dbf235
Qamil Mirza <qamilmirza@gmail.com>
2024-10-30 21:34:24 -0700

    added animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,30 +13,43 @@ import graph from "@/public/graph.gif";
 
 import { BentoCard, BentoGrid } from "@/components/ui/bento-grid";
 import IconCloud from "@/components/ui/icon-cloud";
+import TypingAnimation from "@/components/ui/typing-animation";
+import BlurFade from "@/components/ui/blur-fade";
+import WordPullUp from "@/components/ui/word-pull-up";
 
 // Adjust Bento Content here
 const features = [
   {
     Icon: RocketIcon,
-    name: "Data Science and Applied Mathematics",
+    name: "Data Science Meets Applied Mathematics",
     description: "University of California Berkeley",
     href: "/",
     cta: "Learn more",
-    background: <img src={qm.src} className="absolute inset-0 w-full h-full object-cover object-right-bottom opacity-60" />,
+    background: (
+      <img
+        src={qm.src}
+        className="absolute inset-0 w-full h-full object-cover object-right-bottom opacity-60"
+      />
+    ),
     className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
   },
   {
     Icon: Crosshair2Icon,
-    name: "Modeling Chemical Reaction Networks For Toxicology With Graph Neural Networks",
+    name: "Modeling Chemical Reactions with Graph Neural Networks",
     description: "",
     href: "/",
     cta: "Learn more",
-    background: <img src={graph.src} className="absolute inset-0 w-full h-full object-cover opacity-60" />,
+    background: (
+      <img
+        src={graph.src}
+        className="absolute inset-0 w-full h-full object-cover opacity-60"
+      />
+    ),
     className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
   },
   {
     Icon: GlobeIcon,
-    name: "I Love Building Things",
+    name: "Passionate Builder and Innovator",
     description: "Checkout Some of The Projects I've Worked On Below",
     href: "/",
     cta: "Learn more",
@@ -45,7 +58,7 @@ const features = [
   },
   {
     Icon: Pencil1Icon,
-    name: "I Love Learning and Sharing My Knowledge",
+    name: "Sharing Knowledge, Fueling Growth",
     description: "Checkout My Blog On Medium For More!",
     href: "/",
     cta: "Learn more",
@@ -54,9 +67,8 @@ const features = [
   },
   {
     Icon: LightningBoltIcon,
-    name: "Runner, Climber and Music Enthusiast",
-    description:
-      "A little bit on what I do outside of work",
+    name: "Runner, Climber, and Music Lover",
+    description: "A little bit on what I do outside of work",
     href: "/",
     cta: "Learn more",
     background: <img className="absolute -right-20 -top-20 opacity-60" />,
@@ -112,22 +124,33 @@ export default function Home() {
   return (
     <div className="p-6">
       <div className="flex justify-center p-6">
-        <h1 className="font-bold text-3xl">Hi, I'm Qamil!</h1>
+        <TypingAnimation
+          className="text-4xl font-bold text-black dark:text-white"
+          text="Hi, I'm Qamil!"
+          duration={100}
+        />
       </div>
-      <div>
-        <BentoGrid className="lg:grid-rows-3">
-          {features.map((feature) => (
-            <BentoCard key={feature.name} {...feature} />
-          ))}
-        </BentoGrid>
-      </div>
+      <BlurFade delay={0.25} inView>
+        <div>
+          <BentoGrid className="lg:grid-rows-3">
+            {features.map((feature) => (
+              <BentoCard key={feature.name} {...feature} />
+            ))}
+          </BentoGrid>
+        </div>
+      </BlurFade>
       <div className="flex justify-center p-3">
-        <h1 className="font-bold text-3xl py-3 mt-6">Technologies I've worked with</h1>
+        <WordPullUp
+          className="font-bold text-3xl py-3 mt-6"
+          words="Technologies I've worked with"
+        />
       </div>
       <div className="flex w-full h-[100%] items-center justify-center">
         <IconCloud iconSlugs={slugs} />
       </div>
-      <footer>HI</footer>
+      <div className="p-3 flex justify-center">
+        <h1 className="font-bold text-3xl py-3 mt-6">Let's Get Connected!</h1>
+      </div>
     </div>
   );
 }
