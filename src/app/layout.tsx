81e50dcc83ba999a5dbd62c16c27ee3ff4689a32
Qamil Mirza <qamilmirza@gmail.com>
2024-11-11 19:36:55 -0800

    added google search console meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Qamil Mirza",
   description: "Developed by Qamil Mirza",
+  verification: {
+    google: "6JnU6PmG619-vmgNef2xvtkbYnZ_b8jksYJ0H3k0kU8",
+  }
 };
 
 export default function RootLayout({
