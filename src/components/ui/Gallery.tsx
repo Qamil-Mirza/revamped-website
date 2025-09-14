24a9cd510cfa170ba1ad30d2b903729d626163e9
qamil mirza <qamilmirza@gmail.com>
2025-09-14 13:14:38 -0700

    improvement: use next image component for gallery

--- src/components/ui/Gallery.tsx
+++ src/components/ui/Gallery.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BlurFade } from "@/components/ui/blur-fade";
+import Image from "next/image";
 
 interface GalleryProps {
   city?: string;
@@ -13,10 +14,13 @@ function Gallery({ city, description, images }: GalleryProps) {
       <div className="columns-2 gap-4 sm:columns-3">
         {images.map((imageUrl, idx) => (
           <BlurFade key={imageUrl} delay={0.25 + idx * 0.05} inView>
-            <img
-              className="mb-4 size-full rounded-lg object-contain"
+            <Image
               src={imageUrl}
-              alt={`Random stock image ${idx + 1}`}
+              alt={`${city} image ${idx + 1}`}
+              width={400}
+              height={300}
+              className="mb-4 size-full rounded-lg object-contain"
+              loading="lazy" // This enables lazy loading
             />
           </BlurFade>
         ))}
